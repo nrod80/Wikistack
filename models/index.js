04500b8ec73d69cdf878e258174b34de39e16dca
Alexei Bazhenov <abazhenov@gmail.com>
2016-08-08 14:32:51 -0500

    setup sequelize tables

--- models/index.js
+++ models/index.js
@@ -1,31 +1,49 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
 
 var Page = db.define('page', {
     title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     urlTitle: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     content: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
     },
     status: {
         type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+}, {
+    getterMethods: {
+        url: function() {
+            return "/wiki/" + this.urlTitle
+        }
     }
 });
 
 var User = db.define('user', {
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
     },
     email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        isEmail: true,
+        allowNull: false
     }
 });
 
 module.exports = {
-  Page: Page,
-  User: User
+    Page: Page,
+    User: User
 };
